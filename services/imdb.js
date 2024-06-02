1f8eac843b25dfa721f6fd1f1048e0b0230fd48b
Vitor Silva <vitorsilva10413@gmail.com>
2024-06-03 00:10:27 +0100

    Correção de bugs com a localização do servidor para o imdb

--- services/imdb.js
+++ services/imdb.js
@@ -6,7 +6,8 @@ module.exports = async (id) => {
 
   const res = await axios.get(`https://www.imdb.com/title/${id}`, {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'Accept-Language': 'pt-PT,pt;q=0.9'
     }
   });
 
