27e817738a08405857555b8ac23ad59d7f194265
Vitor Silva <vitorsilva10413@gmail.com>
2024-06-12 02:31:07 +0100

    Criado contador

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { TugaKidsCatalog, TugaKidsStream } = require('./services/tugakids')
 
 const builder = new addonBuilder({
   id: `pt.tugaplay.${process.env.NODE_ENV === 'dev' ? 'developer' : 'stream'}`,
-  version: '1.1.2',
+  version: '1.1.4',
   name: 'TugaPlay',
   description: 'Aceda a uma variedade de filmes e séries, reunidos de diversos serviços de terceiros. Suporte-me livremente: https://coindrop.to/vitorh_asilva',
   contactEmail: 'vitorsilva10413@gmail.com',
@@ -20,29 +20,47 @@ const builder = new addonBuilder({
   idPrefixes: ['tt']
 })
 
+let onlineUsers = 0;
+let totalUsers = 0;
 
 builder.defineStreamHandler(async function (args) {
+  onlineUsers++;
+  totalUsers++;
+  if (onlineUsers > 1)
+    console.log(`🟢 Online Users: ${onlineUsers}`)
+
   let existingStreams = [];
   if (args.type === 'movie') {
     existingStreams = existingStreams.concat(await TugaKidsStream(args.type, args.id))
     // existingStreams = existingStreams.concat(await TugaFlix(args.type, args.id))
+    onlineUsers--;
     return Promise.resolve({
       streams: existingStreams
     })
   } else if (args.type === 'series') {
     // existingStreams = existingStreams.concat(await TugaFlix(args.type, args.id))
+    onlineUsers--;
     return Promise.resolve({ streams: existingStreams })
   } else {
+    onlineUsers--;
     return Promise.resolve({ streams: [] })
   }
 })
 
 builder.defineCatalogHandler(async function (args) {
+  onlineUsers++;
+  totalUsers++;
+
+  if (onlineUsers > 1)
+    console.log(`🟢 Online Users: ${onlineUsers}`)
+
   let existingCatalogs = [];
   if (args.type === 'movie') {
     existingCatalogs = existingCatalogs.concat(await TugaKidsCatalog(args.type, args.id))
+    onlineUsers--;
     return Promise.resolve({ metas: existingCatalogs })
   } else {
+    onlineUsers--;
     return Promise.resolve({ metas: [] })
   }
 })
@@ -52,4 +70,6 @@ serveHTTP(builder.getInterface(), { port: process.env.PORT || 7000 })
 
 setInterval(() => {
   https.get('https://tugaplay.onrender.com/manifest.json')
+  totalUsers--;
+  console.log(`🧮 Total Users ${totalUsers}`);
 }, 1000 * 60)
