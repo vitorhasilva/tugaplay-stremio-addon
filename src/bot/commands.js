eadc04bb4d016285dd18d9cd82c47a624e8cbb26
Vitor Silva <vitorsilva10413@gmail.com>
2024-09-11 21:29:17 +0100

    Criado comando de verificação pelo bot

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,13 +1,38 @@
+const supportRoleId = process.env.DISCORD_ROLE_SUPPORT_ID;
 module.exports = (app) => [
   {
     data: {
-      name: 'olá',
-      description: 'Diz olá!',
+      name: 'verificaruser',
+      description: 'Fazer a verificação de email, caso o utilizador não consiga.',
+      options: [
+        {
+          name: 'token',
+          type: 3,
+          description: 'Token de Verificação',
+          required: true,
+        },
+      ],
     },
     async execute(interaction) {
-      await interaction.reply('Olá! Como estás?');
-      // eslint-disable-next-line no-useless-escape
-      console.log('%csrc\bot\commands.js:11 app.env', 'color: #007acc;', app.env);
+      const supportRole = interaction.guild.roles.cache.get(supportRoleId);
+      const memberRoles = interaction.member.roles.cache;
+      if (!memberRoles.has(supportRoleId)) {
+        interaction.reply({
+          content: `Apenas utilizadores com o cargo \`${supportRole.name}\` podem usar este comando.`,
+          ephemeral: true,
+        });
+      } else {
+        const token = interaction.options.getString('token');
+        app.services.auth.verify(token, '0.0.0.0', `Suporte de Discord (${interaction.member.nickname || interaction.user.username})`)
+          .then(() => interaction.reply({
+            content: 'Utilizador verificado com sucesso!',
+            ephemeral: true,
+          }))
+          .catch((err) => interaction.reply({
+            content: err.message,
+            ephemeral: true,
+          }));
+      }
     },
   },
   // Adiciona mais comandos aqui seguindo a mesma estrutura
